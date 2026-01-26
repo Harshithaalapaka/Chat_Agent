dda59cf169d4eb613ee287f5c0820d4c6abe97a4
Harshithaalapaka <harshithaalapaka@gmail.com>
2026-01-26 09:46:25 +0530

    Initialised

---
+++ agent/llm.py
@@ -0,0 +1,23 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv  
+import os
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-pro",
+    google_api_key=GEMINI_API_KEY,
+    temperature=0.3,
+    max_op_tokens=1024
+)
+
+def chat(user_input: str) -> str:
+    prompt = f"""
+You are a polite and professional customer support assistant.
+Be clear, concise, and helpful.
+
+User: {user_input}
+Assistant:
+"""
+    response=llm.invoke(prompt)
+    return response.content
